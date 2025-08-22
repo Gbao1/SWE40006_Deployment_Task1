b360793423f30483c5c95331b5e7179c573258b1
Gbao1 <80185734+Gbao1@users.noreply.github.com>
2025-08-22 18:29:26 +1000

    Add files via upload

---
+++ Task 1.3/ClockDLL/Clock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    public class Clock
+    {
+        Counter _hour = new Counter("hour");
+        Counter _minute = new Counter("minute");
+        Counter _second = new Counter("second");
+
+        public void Ticks()
+        {
+            _second.Increment();
+            if (_second.Ticks > 59)
+            {
+                _second.Reset();
+                _minute.Increment();
+                if (_minute.Ticks > 59)
+                {
+                    _minute.Reset();
+                    _hour.Increment();
+                    if (_hour.Ticks > 23)
+                    {
+                        Reset();
+                    }
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            _second.Reset();
+            _minute.Reset();
+            _hour.Reset();
+        }
+
+        public void SetTime(string t)
+        {
+            string[] array = t.Split(":");
+
+            _hour = new Counter("hour", int.Parse(array[0]));
+            _minute = new Counter("minute", int.Parse(array[1]));
+            _second = new Counter("second", int.Parse(array[2]));
+        }
+
+        public string CurrentTime()
+        {
+            return $"{_hour.Ticks:D2}:{_minute.Ticks:D2}:{_second.Ticks:D2}"; //At least 2 digits
+        }
+    }
+
+}
---
+++ Task 1.3/ClockDLL/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Clock
+{
+    public class Counter
+    {
+        private int _count;
+        private string _name;
+
+        public Counter(string name)
+        {
+            _name = name;
+            _count = 0;
+        }
+
+        public Counter(string name, int count)
+        {
+            _name = name;
+            _count = count;
+        }
+
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public void Reset()
+        {
+            _count = 0;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public int Ticks
+        {
+            get
+            {
+                return _count;
+            }
+        }
+    }
+}
