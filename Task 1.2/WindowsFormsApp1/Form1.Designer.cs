129a1244ae312da24833b09b60a615d8df645c9b
Gbao1 <80185734+Gbao1@users.noreply.github.com>
2025-08-22 14:34:26 +1000

    Add files via upload

---
+++ Task 1.2/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelClock;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonReset;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.labelClock = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelClock
+            // 
+            this.labelClock.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.labelClock.Location = new System.Drawing.Point(57, 32);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(229, 53);
+            this.labelClock.TabIndex = 0;
+            this.labelClock.Text = "00:00:00";
+            this.labelClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelClock.Click += new System.EventHandler(this.labelClock_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(34, 107);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(86, 32);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(137, 107);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(86, 32);
+            this.buttonPause.TabIndex = 2;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(240, 107);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(86, 32);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 171);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelClock);
+            this.Name = "Form1";
+            this.Text = "Clock Counter";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Task 1.2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private int elapsedSeconds = 0;
+        private bool isRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Setup timer
+            timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += Timer_Tick;
+
+            // Initialize UI text
+            labelClock.Text = "00:00:00";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedSeconds++;
+            TimeSpan ts = TimeSpan.FromSeconds(elapsedSeconds);
+            labelClock.Text = ts.ToString(@"hh\:mm\:ss");
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                timer.Start();
+                isRunning = true;
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                timer.Stop();
+                isRunning = false;
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            isRunning = false;
+            elapsedSeconds = 0;
+            labelClock.Text = "00:00:00";
+        }
+
+        private void labelClock_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
