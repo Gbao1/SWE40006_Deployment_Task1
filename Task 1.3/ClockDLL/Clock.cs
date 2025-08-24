a180db28178edbf9081fcc3c0e2273284d690140
Gbao1 <80185734+Gbao1@users.noreply.github.com>
2025-08-24 13:35:48 +1000

    Update Clock.cs

--- Task 1.3/ClockDLL/Clock.cs
+++ Task 1.3/ClockDLL/Clock.cs
@@ -1,56 +1,29 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
 
-namespace Clock
+namespace ClockDLL
 {
-    public class Clock
+    public class ClockCounter
     {
-        Counter _hour = new Counter("hour");
-        Counter _minute = new Counter("minute");
-        Counter _second = new Counter("second");
+        private int ElapsedSeconds { get; set; }
+        private bool Running { get; set; }
 
-        public void Ticks()
+        public void Start() => Running = true;
+        public void Stop() => Running = false;
+        public void Reset() { ElapsedSeconds = 0; Running = false; }
+
+        public void Tick()
         {
-            _second.Increment();
-            if (_second.Ticks > 59)
-            {
-                _second.Reset();
-                _minute.Increment();
-                if (_minute.Ticks > 59)
-                {
-                    _minute.Reset();
-                    _hour.Increment();
-                    if (_hour.Ticks > 23)
-                    {
-                        Reset();
-                    }
-                }
-            }
+            if (Running) ElapsedSeconds++;
         }
 
-        public void Reset()
+        public string GetTimeString()
         {
-            _second.Reset();
-            _minute.Reset();
-            _hour.Reset();
-        }
-
-        public void SetTime(string t)
-        {
-            string[] array = t.Split(":");
-
-            _hour = new Counter("hour", int.Parse(array[0]));
-            _minute = new Counter("minute", int.Parse(array[1]));
-            _second = new Counter("second", int.Parse(array[2]));
-        }
-
-        public string CurrentTime()
-        {
-            return $"{_hour.Ticks:D2}:{_minute.Ticks:D2}:{_second.Ticks:D2}"; //At least 2 digits
+            return TimeSpan.FromSeconds(ElapsedSeconds).ToString(@"hh\:mm\:ss");
         }
     }
-
 }
+
